4a16250dc9a6ebc5bf07968f29a01c760d675759
Luiza <luizabrum06@gmail.com>
2026-06-02 17:42:00 -0300

    feat: implement ReportController.java, implement and add ReportView.java

--- src/main/java/org/unicamp/poo/controller/ReportController.java
+++ src/main/java/org/unicamp/poo/controller/ReportController.java
@@ -1,4 +1,116 @@
 package org.unicamp.poo.controller;
 
+import org.unicamp.poo.dao.TransactionDAO;
+import org.unicamp.poo.dao.WalletDAO;
+import org.unicamp.poo.model.Transaction;
+import org.unicamp.poo.model.Wallet;
+import org.unicamp.poo.model.enums.OperationType;
+import org.unicamp.poo.util.ConsoleScanner;
+import org.unicamp.poo.util.MessageProvider;
+import org.unicamp.poo.view.Menu;
+import org.unicamp.poo.view.ReportView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportController {
+    private final WalletDAO walletDAO;
+    private final TransactionDAO transactionDAO;
+    private final ReportView view;
+    private final MessageProvider messages;
+
+    public ReportController(WalletDAO walletDAO, TransactionDAO transactionDAO, ReportView view, MessageProvider messages) {
+        super();
+        this.walletDAO = walletDAO;
+        this.transactionDAO = transactionDAO;
+        this.view = view;
+        this.messages = messages;
+    }
+
+    private double calculateWalletBalance (int walletId){
+        List<Transaction> transactions = transactionDAO.findByWalletId(walletId);
+        double balance = 0.0;
+
+        for (Transaction t : transactions){
+            if (t.getOperationType() == OperationType.CASH_IN) {
+                balance += t.getQuantity(); // Increases balance on buy
+            }
+            else if (t.getOperationType() == OperationType.CASH_OUT){
+                balance -= t.getQuantity(); // Decreases balance on sell
+            }
+        }
+        return balance;
+    }
+
+    public void showFinancialReport(){
+        int walletId = view.readWalletIdReport();
+
+        Wallet wallet = walletDAO.findById(walletId);
+        if (wallet == null){
+            view.showErrorMessage(messages.get("transaction.wallet.notFound"));
+        return;
+        }
+
+        List<Transaction> transactions = transactionDAO.findByWalletId(walletId);
+        double totalCashIn = 0.0;
+        double totalCashOut = 0.0;
+
+        for(Transaction t : transactions){
+            if (t.getOperationType() == OperationType.CASH_IN) {
+                totalCashIn += t.getQuantity();
+            }
+            else if (t.getOperationType() == OperationType.CASH_OUT){
+                totalCashOut += t.getQuantity();
+            }
+        }
+
+        double result = totalCashIn - totalCashOut;
+
+        view.showFinancialReport(walletId, totalCashIn, totalCashOut, result);
+    }
+
+    private void showSortedWallets(){
+        List<Wallet> wallets = walletDAO.findAll();
+
+        if(wallets == null || wallets.isEmpty()){
+            view.showErrorMessage(messages.get("report.wallets.empty"));
+            return;
+        }
+
+        wallets.sort ((w1, w2) -> {
+            double balance1 = calculateWalletBalance(w1.getId());
+            double balance2 = calculateWalletBalance(w2.getId());
+            return Double.compare(balance2, balance1);
+        });
+
+        double[] sortedBalances = new double [wallets.size()];
+        for (int i = 0; i < wallets.size(); i++){
+            sortedBalances[i] = calculateWalletBalance(wallets.get(i).getId());
+        }
+
+        view.showSortedWalletsReport(wallets, sortedBalances);
+    }
+
+    private List<String> getMenuOptions(){
+        final List<String> options = new ArrayList<>();
+        options.add(messages.get("reportMenu.return"));
+        options.add(messages.get("reportMenu.financial"));
+        options.add(messages.get("reportMenu.sortedWallets"));
+        return options;
+    }
+    public void start() {
+        final List<String> options = getMenuOptions();
+        final Menu reportMenu = new Menu(ConsoleScanner.getInstance());
+        boolean loop = true;
+
+        while (loop) {
+            switch (reportMenu.getChoice(messages.get("reportMenu.title"), options, messages.get("reportMenu.prompt"))) {
+                case 0 -> loop = false;
+                case 1 -> showFinancialReport();
+                case 2 -> showSortedWallets();
+                default -> loop = false;
+            }
+        }
+    }
+
 }
---
+++ src/main/java/org/unicamp/poo/view/ReportView.java
@@ -0,0 +1,68 @@
+package org.unicamp.poo.view;
+
+import org.unicamp.poo.model.Wallet;
+import org.unicamp.poo.util.ConsoleScanner;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ReportView {
+
+    // ANSI Escape codes for coloring console output texts
+    public static final String reset = "\u001B[0m";
+    public static final String green = "\u001B[32m";
+    public static final String red = "\u001B[31m";
+
+    public int readWalletIdReport(){
+        Scanner read = ConsoleScanner.getInstance();
+
+        System.out.print("Digite o ID da carteira para gerar o relatório financeiro: ");
+        int id = read.nextInt();
+        read.nextLine();
+
+        return id;
+    }
+
+    public void showFinancialReport(int walletId, double totalCashIn, double totalCashOut, double result) {
+
+        System.out.println("\n----------------------------------");
+        System.out.println("       RELATÓRIO FINANCEIRO       ");
+        System.out.println("----------------------------------");
+        System.out.println("ID da carteira: " + walletId);
+        System.out.printf("Total de entrada de moedas: %.2f%n", totalCashIn);
+        System.out.printf("Total de saídas de moedas: %.2f%n", totalCashOut);
+        System.out.println("----------------------------------");
+        System.out.print("Resultado Líquido: ");
+
+        if (result < 0){
+            System.out.println(red + String.format("%.2f", result) + reset);
+        }
+        else {
+            System.out.println(green + String.format("%.2f", result) + reset);
+        }
+        System.out.println("--------------------------------");
+
+    }
+
+    public void showSortedWalletsReport(List<Wallet> wallets, double[] sortedBalances) {
+        System.out.println("\n---------------------------------");
+        System.out.println("       CARTEIRAS POR SALDO        ");
+        System.out.println("---------------------------------");
+
+        System.out.printf("%-10s | %-25s | %-15s%n", "ID", "PROPRIETÁRIO", "SALDO ATUAL");
+        System.out.println("---------------------------------");
+
+        for(int i = 0; i < wallets.size(); i++){
+            Wallet w = wallets.get(i);
+            double balance = sortedBalances[i];
+
+            System.out.printf("%-10d | %-25s | %.2f%n", w.getId(), w.getHolder(), balance);
+        }
+        System.out.println("---------------------------------");
+    }
+
+    // Prints an error message highlighted in red color
+    public void showErrorMessage(String s) {
+        System.out.println(red + s + reset);
+    }
+}
